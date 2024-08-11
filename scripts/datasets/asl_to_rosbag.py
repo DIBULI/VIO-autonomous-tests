bc73c1e72dc7706934435ec3ee4a521c2f6bb7b0
Xiangqin Chen <rex@mail.leafwolf.net>
2024-08-11 15:43:36 -0700

    update asl script

--- scripts/datasets/asl_to_rosbag.py
+++ scripts/datasets/asl_to_rosbag.py
@@ -34,94 +34,129 @@ def find_sensor_folders(dataset_path, sensor_type):
                     sensor_folders.append(folder)
     return sensor_folders
 
+def process_camera_data(bag, dataset_path, camera_folders, bridge, verbose):
+    for cam in camera_folders:
+        cam_path = os.path.join(dataset_path, cam)
+        cam_data = read_csv(os.path.join(cam_path, 'data.csv'))
+        cam_info = load_yaml(os.path.join(cam_path, 'sensor.yaml'))
+        
+        if verbose:
+            print(f"Processing {cam} data from {cam_path}...")
+
+        for idx, row in cam_data.iterrows():
+            timestamp_ns = row.iloc[0]
+            timestamp = rospy.Time.from_sec(int(timestamp_ns) * 1e-9)
+            image_path = os.path.join(cam_path, 'data', row['filename'])
+            image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+            image_msg = bridge.cv2_to_imgmsg(image, encoding="mono8")
+            image_msg.header.stamp = timestamp
+            image_msg.header.frame_id = cam
+            bag.write(f'/{cam}/image_raw', image_msg, timestamp)
+
+def process_imu_data(bag, dataset_path, imu_folders, verbose):
+    for imu in imu_folders:
+        imu_path = os.path.join(dataset_path, imu)
+        imu_data = read_csv(os.path.join(imu_path, 'data.csv'))
+        imu_info = load_yaml(os.path.join(imu_path, 'sensor.yaml'))
+        
+        if verbose:
+            print(f"Processing {imu} data from {imu_path}...")
+
+        for idx, row in imu_data.iterrows():
+            timestamp_ns = row.iloc[0]
+            timestamp = rospy.Time.from_sec(int(timestamp_ns) * 1e-9)
+            imu_msg = Imu()
+            imu_msg.header.stamp = timestamp
+            imu_msg.header.frame_id = imu
+            imu_msg.angular_velocity.x = row['w_RS_S_x [rad s^-1]']
+            imu_msg.angular_velocity.y = row['w_RS_S_y [rad s^-1]']
+            imu_msg.angular_velocity.z = row['w_RS_S_z [rad s^-1]']
+            imu_msg.linear_acceleration.x = row['a_RS_S_x [m s^-2]']
+            imu_msg.linear_acceleration.y = row['a_RS_S_y [m s^-2]']
+            imu_msg.linear_acceleration.z = row['a_RS_S_z [m s^-2]']
+            bag.write(f'/{imu}', imu_msg, timestamp)
+
+def process_leica_data(bag, dataset_path, leica_folders, verbose):
+    for leica in leica_folders:
+        leica_path = os.path.join(dataset_path, leica)
+        leica_data = read_csv(os.path.join(leica_path, 'data.csv'))
+        
+        if verbose:
+            print(f"Processing {leica} data from {leica_path}...")
+
+        for idx, row in leica_data.iterrows():
+            timestamp_ns = row.iloc[0]
+            timestamp = rospy.Time.from_sec(int(timestamp_ns) * 1e-9)
+            point_msg = PointStamped()
+            point_msg.header.stamp = timestamp
+            point_msg.header.frame_id = leica
+            point_msg.point.x = row['p_RS_R_x [m]']
+            point_msg.point.y = row['p_RS_R_y [m]']
+            point_msg.point.z = row['p_RS_R_z [m]']
+            bag.write(f'/{leica}', point_msg, timestamp)
+
+def process_groundtruth_data(bag, dataset_path, groundtruth_folders, verbose):
+    for gt in groundtruth_folders:
+        gt_path = os.path.join(dataset_path, gt)
+        gt_data = read_csv(os.path.join(gt_path, 'data.csv'))
+        
+        if verbose:
+            print(f"Processing {gt} data from {gt_path}...")
+
+        for idx, row in gt_data.iterrows():
+            timestamp_ns = row.iloc[0]
+            timestamp = rospy.Time.from_sec(int(timestamp_ns) * 1e-9)
+            odom_msg = Odometry()
+            odom_msg.header.stamp = timestamp
+            odom_msg.header.frame_id = gt
+            odom_msg.pose.pose.position.x = row['p_RS_R_x [m]']
+            odom_msg.pose.pose.position.y = row['p_RS_R_y [m]']
+            odom_msg.pose.pose.position.z = row['p_RS_R_z [m]']
+            odom_msg.pose.pose.orientation.w = row['q_RS_w []']
+            odom_msg.pose.pose.orientation.x = row['q_RS_x []']
+            odom_msg.pose.pose.orientation.y = row['q_RS_y []']
+            odom_msg.pose.pose.orientation.z = row['q_RS_z []']
+            bag.write(f'/{gt}', odom_msg, timestamp)
+
+def process_vicon_data(bag, dataset_path, vicon_folders, verbose):
+    for vicon in vicon_folders:
+        vicon_path = os.path.join(dataset_path, vicon)
+        vicon_data = read_csv(os.path.join(vicon_path, 'data.csv'))
+        
+        if verbose:
+            print(f"Processing {vicon} data from {vicon_path}...")
+
+        for idx, row in vicon_data.iterrows():
+            timestamp_ns = row.iloc[0]
+            timestamp = rospy.Time.from_sec(int(timestamp_ns) * 1e-9)
+            odom_msg = Odometry()
+            odom_msg.header.stamp = timestamp
+            odom_msg.header.frame_id = vicon
+            odom_msg.pose.pose.position.x = row['p_RS_R_x [m]']
+            odom_msg.pose.pose.position.y = row['p_RS_R_y [m]']
+            odom_msg.pose.pose.position.z = row['p_RS_R_z [m]']
+            odom_msg.pose.pose.orientation.w = row['q_RS_w []']
+            odom_msg.pose.pose.orientation.x = row['q_RS_x []']
+            odom_msg.pose.pose.orientation.y = row['q_RS_y []']
+            odom_msg.pose.pose.orientation.z = row['q_RS_z []']
+            bag.write(f'/{vicon}', odom_msg, timestamp)
+
 def create_bag(output_bag, dataset_path, verbose=False):
     bridge = CvBridge()
     bag = rosbag.Bag(output_bag, 'w')
     
     try:
         camera_folders = find_sensor_folders(dataset_path, 'camera')
-        for cam in camera_folders:
-            cam_path = os.path.join(dataset_path, cam)
-            cam_data = read_csv(os.path.join(cam_path, 'data.csv'))
-            cam_info = load_yaml(os.path.join(cam_path, 'sensor.yaml'))
-            
-            if verbose:
-                print(f"Processing {cam} data from {cam_path}...")
-
-            for idx, row in cam_data.iterrows():
-                timestamp_ns = row.iloc[0]
-                timestamp = rospy.Time.from_sec(int(timestamp_ns) * 1e-9)
-                image_path = os.path.join(cam_path, 'data', row['filename'])
-                image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
-                image_msg = bridge.cv2_to_imgmsg(image, encoding="mono8")
-                image_msg.header.stamp = timestamp
-                image_msg.header.frame_id = cam
-                bag.write(f'/{cam}/image_raw', image_msg, timestamp)
-        
         imu_folders = find_sensor_folders(dataset_path, 'imu')
-        for imu in imu_folders:
-            imu_path = os.path.join(dataset_path, imu)
-            imu_data = read_csv(os.path.join(imu_path, 'data.csv'))
-            imu_info = load_yaml(os.path.join(imu_path, 'sensor.yaml'))
-            
-            if verbose:
-                print(f"Processing {imu} data from {imu_path}...")
-
-            for idx, row in imu_data.iterrows():
-                timestamp_ns = row.iloc[0]
-                timestamp = rospy.Time.from_sec(int(timestamp_ns) * 1e-9)
-                imu_msg = Imu()
-                imu_msg.header.stamp = timestamp
-                imu_msg.header.frame_id = imu
-                imu_msg.angular_velocity.x = row['w_RS_S_x [rad s^-1]']
-                imu_msg.angular_velocity.y = row['w_RS_S_y [rad s^-1]']
-                imu_msg.angular_velocity.z = row['w_RS_S_z [rad s^-1]']
-                imu_msg.linear_acceleration.x = row['a_RS_S_x [m s^-2]']
-                imu_msg.linear_acceleration.y = row['a_RS_S_y [m s^-2]']
-                imu_msg.linear_acceleration.z = row['a_RS_S_z [m s^-2]']
-                bag.write(f'/{imu}', imu_msg, timestamp)
-        
         leica_folders = find_sensor_folders(dataset_path, 'position')
-        for leica in leica_folders:
-            leica_path = os.path.join(dataset_path, leica)
-            leica_data = read_csv(os.path.join(leica_path, 'data.csv'))
-            
-            if verbose:
-                print(f"Processing {leica} data from {leica_path}...")
-
-            for idx, row in leica_data.iterrows():
-                timestamp_ns = row.iloc[0]
-                timestamp = rospy.Time.from_sec(int(timestamp_ns) * 1e-9)
-                point_msg = PointStamped()
-                point_msg.header.stamp = timestamp
-                point_msg.header.frame_id = leica
-                point_msg.point.x = row['p_RS_R_x [m]']
-                point_msg.point.y = row['p_RS_R_y [m]']
-                point_msg.point.z = row['p_RS_R_z [m]']
-                bag.write(f'/{leica}', point_msg, timestamp)
-        
         groundtruth_folders = find_sensor_folders(dataset_path, 'visual-inertial')
-        for gt in groundtruth_folders:
-            gt_path = os.path.join(dataset_path, gt)
-            gt_data = read_csv(os.path.join(gt_path, 'data.csv'))
-            
-            if verbose:
-                print(f"Processing {gt} data from {gt_path}...")
-
-            for idx, row in gt_data.iterrows():
-                timestamp_ns = row.iloc[0]
-                timestamp = rospy.Time.from_sec(int(timestamp_ns) * 1e-9)
-                odom_msg = Odometry()
-                odom_msg.header.stamp = timestamp
-                odom_msg.header.frame_id = gt
-                odom_msg.pose.pose.position.x = row['p_RS_R_x [m]']
-                odom_msg.pose.pose.position.y = row['p_RS_R_y [m]']
-                odom_msg.pose.pose.position.z = row['p_RS_R_z [m]']
-                odom_msg.pose.pose.orientation.w = row['q_RS_w []']
-                odom_msg.pose.pose.orientation.x = row['q_RS_x []']
-                odom_msg.pose.pose.orientation.y = row['q_RS_y []']
-                odom_msg.pose.pose.orientation.z = row['q_RS_z []']
-                bag.write(f'/{gt}', odom_msg, timestamp)
+        vicon_folders = find_sensor_folders(dataset_path, 'pose')
+        
+        process_camera_data(bag, dataset_path, camera_folders, bridge, verbose)
+        process_imu_data(bag, dataset_path, imu_folders, verbose)
+        process_leica_data(bag, dataset_path, leica_folders, verbose)
+        process_groundtruth_data(bag, dataset_path, groundtruth_folders, verbose)
+        process_vicon_data(bag, dataset_path, vicon_folders, verbose)
 
     finally:
         bag.close()
