bdf52d2284784913bcf531659bae8e84ece5241b
Xiangqin Chen <rex@mail.leafwolf.net>
2024-08-12 09:54:26 -0700

    added support for vins fusion auto test

---
+++ scripts/repos/vins-fusion-init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+current_directory="$(cd "$(dirname "$0")" && pwd)"
+project_root_dir=${current_directory}/../..
+
+mkdir -p ${project_root_dir}/workspace/src && cd ${project_root_dir}/workspace
+
+cd ${current_directory}/third_party
+mkdir -p ceres_solver && cd ceres_solver
+git clone https://ceres-solver.googlesource.com/ceres-solver
+cd ceres-solver
+git checkout 1.14.0
+mkdir build && cd build
+cmake ..
+make -j8
+sudo make install
+
+cd ${project_root_dir}/workspace/src
+git clone https://github.com/HalfVulpes/vins-fusion-cv4.git
+cd ../
+catkin_make
+
+source ${project_root_dir}/workspace/devel/setup.bash
+
+echo "source ${project_root_dir}/workspace/devel/setup.bash" >> ~/.bashrc
+echo "VINS-Fusion initialization completed."
---
+++ scripts/tests/vins-fusion-tests.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+# Set the current directory and project root directory
+current_directory="$(cd "$(dirname "$0")" && pwd)"
+project_root_dir=${current_directory}/../..
+
+DATASET_PATH=$DATASET_PATH
+TEST_RESULT_PATH=${project_root_dir}/tests_result
+
+mkdir -p ${TEST_RESULT_PATH} && cd ${TEST_RESULT_PATH}
+
+rm -rf vins-fusion-tests
+mkdir -p vins-fusion-tests
+cd vins-fusion-tests
+
+roscore &
+ROSCORE_PID=$!
+
+sleep 2
+
+EUROC_DATASET_PATH=${DATASET_PATH}/euroc
+EUROC_SCENARIOS=("machine_hall" "vicon_room1" "vicon_room2")
+
+EUROC_BAGS=()
+
+for SCENARIO in "${EUROC_SCENARIOS[@]}"; do
+    while IFS= read -r -d '' file; do
+        EUROC_BAGS+=("$file")
+    done < <(find "${EUROC_DATASET_PATH}/$SCENARIO" -name "*.bag" -type f -print0)
+done
+
+echo "Found bags:"
+for BAG in "${EUROC_BAGS[@]}"; do
+    echo "$BAG"
+done
+
+mkdir -p euroc
+for BAG in "${EUROC_BAGS[@]}"; do
+  echo "-------------------------- processing dataset: ${BAG} ------------------------------"
+  SEQUENCE_NAME_WITH_EXT=$(basename "$BAG")
+  SEQUENCE_NAME="${SEQUENCE_NAME_WITH_EXT%.*}"
+
+  mkdir -p euroc/${SEQUENCE_NAME}
+  cd euroc/${SEQUENCE_NAME}
+  
+  echo -e "458.654 457.296 367.215 248.375 -0.28340811 0.07395907 0.00019359 1.76187114e-05\n752 480\ncrop\n640 480\n" > camera.txt
+
+  rosrun vins vins_node ~/catkin_ws/src/VINS-Fusion/config/euroc/euroc_stereo_imu_config.yaml \
+    > ${SEQUENCE_NAME}.output &
+  VINS_FUSION_PID=$!
+
+  sleep 5
+
+  echo "Start to record the odometry result"
+  rosbag record -O odom_result /vins_estimator/odometry > rosbag.output 2>&1 &
+  ROSBAG_RECORD_PID=$!
+  sleep 1
+
+  echo "Start to play the dataset bag"
+  rosbag play ${BAG} &
+  ROSBAG_PLAY_PID=$!
+
+  CSV_FILE=system.csv
+  touch ${CSV_FILE}
+
+  while ps -p $ROSBAG_PLAY_PID > /dev/null 2>&1; do
+    TIMESTAMP=$(date +%s%3N)
+    CPU_MEM_USAGE=$(ps -p $VINS_FUSION_PID -o %cpu,%mem --no-headers)
+
+    if [ -n "$CPU_MEM_USAGE" ]; then
+        echo "$TIMESTAMP $CPU_MEM_USAGE" >> "$CSV_FILE"
+    fi
+
+    sleep 0.1
+  done
+
+  sleep 5
+  echo "Killing rosbag recording process"
+  while kill -SIGINT $ROSBAG_RECORD_PID 2>/dev/null; do
+    echo "Waiting for process $ROSBAG_RECORD_PID to be killed..."
+    sleep 1
+  done
+  echo "Killing VINS-Fusion process"
+  kill $VINS_FUSION_PID
+
+  python3 ${current_directory}/python/cal_rosbag_frequency.py odom_result.bag /vins_estimator/odometry > odom_result.hz
+
+  ln -s ${BAG} ${SEQUENCE_NAME}.bag
+  rosbag-merge --outbag_name result --topics /vins_estimator/odometry /vins_estimator/ground_truth --write_bag
+  evo_traj bag result.bag /vins_estimator/odometry --save_plot plot.pdf --full_check -as --ref /vins_estimator/ground_truth
+  evo_ape bag result.bag /vins_estimator/ground_truth /vins_estimator/odometry -as
+  evo_rpe bag result.bag /vins_estimator/ground_truth /vins_estimator/odometry -as
+
+  cd -
+done
+
+# Kill
+kill $ROSCORE_PID
